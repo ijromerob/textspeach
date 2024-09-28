a6a7f6eaf7ab17c6423bb7cc139506cd219e6065
ijromerob <ijromerob@gmail.com>
2024-09-28 11:51:33 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import NavBar from './NavBar';
+import './App.css';
+import TextRender from './TextRender';
+
+function App() {
+  return (
+    <>
+      {/* <NavBar /> */}
+      <TextRender />
+    </>
+  );
+}
+
+export default App;
---
+++ src/TextRender.jsx
@@ -0,0 +1,9 @@
+// This would be the area where the text will be rendered
+import { useState } from 'react';
+
+function TextRender() {
+  const [text, setText] = useState('');
+
+  return <div>TextRender</div>;
+}
+export default TextRender;
